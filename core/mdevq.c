dd19ffa5b76396b2d77b5b671d6812a0a19fa661
Stjepan Poljak <stjepan.poljak@gmail.com>
2021-04-10 16:51:08 +0200

    Added event queue code.

---
+++ core/mdevq.c
@@ -0,0 +1,104 @@
+#include "mdevq.h"
+
+#include <stdlib.h>
+#include <errno.h>
+#include <stdbool.h>
+
+#include "mdll.h"
+#include "mdlog.h"
+
+static bool evq_glob_run;
+static int evq_glob_error;
+
+int md_evq_init(md_evq_t* evq) {
+
+	__ll_init(evq->ll_take_in);
+	__ll_init(evq->ll_take_out);
+
+	return 0;
+}
+
+int md_evq_add_event(md_evq_t* evq, md_event_type_t evtype,
+		     md_event_cb cb, void* data) {
+	md_evqll_t** curr_evq;
+	md_evqll_t* new_ev;
+	md_evqll_t* temp;
+
+	switch (evtype) {
+	case MD_EVENT_RUN_ON_TAKE_IN:
+		curr_evq = &(evq->ll_take_in);
+		break;
+
+	case MD_EVENT_RUN_ON_TAKE_OUT:
+		curr_evq = &(evq->ll_take_out);
+		break;
+
+	default:
+		return -EINVAL;
+	}
+
+	new_ev = malloc(sizeof(*new_ev));
+	if (!new_ev) {
+		md_error("Could not allocate memory.");
+		return -ENOMEM;
+	}
+
+	new_ev->event.callback = cb;
+	new_ev->event.data = data;
+	new_ev->next = NULL;
+
+	__ll_add(*curr_evq, new_ev, temp);
+
+	return 0;
+}
+
+static void md_evq_free(md_evqll_t* curr) {
+	md_evqll_t* temp;
+	int error;
+
+	if (evq_glob_run) {
+		error = curr->event.callback(curr->event.data);
+		if (error && !evq_glob_error)
+			evq_glob_error = error;
+	}
+
+	temp = curr;
+	free(temp);
+
+	return;
+}
+
+int md_evq_run_queue(md_evq_t* evq, md_event_type_t type) {
+	md_evqll_t* curr;
+	md_evqll_t* next;
+
+	evq_glob_run = true;
+	evq_glob_error = 0;
+
+	switch (type) {
+	case MD_EVENT_RUN_ON_TAKE_IN:
+		__ll_deinit(evq->ll_take_in, md_evq_free, curr, next);
+		break;
+	case MD_EVENT_RUN_ON_TAKE_OUT:
+		__ll_deinit(evq->ll_take_out, md_evq_free, curr, next);
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	return evq_glob_error;
+}
+
+int md_evq_deinit(md_evq_t* evq) {
+	md_evqll_t* curr;
+	md_evqll_t* next;
+
+	evq_glob_run = false;
+	evq_glob_error = 0;
+
+	__ll_deinit(evq->ll_take_in, md_evq_free, curr, next);
+	__ll_deinit(evq->ll_take_out, md_evq_free, curr, next);
+
+	return evq_glob_error;
+}
+
--- include/mdevq.h
+++ include/mdevq.h
@@ -1,13 +1,31 @@
 #ifndef MD_EVQ_H
 #define MD_EVQ_H
 
+typedef enum {
+	MD_EVENT_RUN_ON_TAKE_IN,
+	MD_EVENT_RUN_ON_TAKE_OUT
+} md_event_type_t;
+
+typedef int(*md_event_cb)(void*);
+
 typedef struct {
-	void*(*callback)(void*);
+	md_event_cb callback;
 	void* data;
 } md_event_t;
 
-int md_evq_init(void);
-int md_evq_loop(void);
-int md_evq_send(md_event_t*);
+typedef struct md_evqll_t {
+	md_event_t event;
+	struct md_evqll_t* next;
+} md_evqll_t;
+
+typedef struct {
+	md_evqll_t* ll_take_in;
+	md_evqll_t* ll_take_out;
+} md_evq_t;
+
+int md_evq_init(md_evq_t*);
+int md_evq_add_event(md_evq_t*, md_event_type_t, md_event_cb, void*);
+int md_evq_run_queue(md_evq_t*, md_event_type_t);
+int md_evq_deinit(md_evq_t*);
 
 #endif
